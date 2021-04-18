6eec9178db3148ed56f5084add4d3bba54a61beb
LilJuiceBox491 <74076575+LilJuiceBox491@users.noreply.github.com>
2021-04-17 23:35:14 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,69 @@
+# ScratchAuth
+
+A next-generation alternative to [ScratchVerifier](http://scratchverifier.ddns.net:8888/site) and [SV2](http://sv2-server.herokuapp.com/).  A public server is available at https://scratchauth.liljuicebox491.repl.co.
+
+## API Reference
+
+### `GET /api`
+
+This requests the status of the server.
+
+**Good response**:
+
+```sh
+200 OK
+```
+
+**Bad response**:
+
+```sh
+500 Server Error
+```
+
+### `POST /api/verify`
+
+This requests a user's profile to be checked for the code.
+
+**Body:**
+
+```json
+{
+  "user": "scratch_username"
+}
+```
+
+**Good response**:
+
+```json
+{
+  "code": "3Q8Q8UqObPEQocs3UiVHXy2fp2QqcoQVtlu5seN3nb5BZauFTz"
+}
+```
+
+**Bad response**:
+
+```sh
+406 Not Acceptable
+```
+
+### `POST /api/verify`
+
+**Body:**
+
+```json
+{
+  "user": "scratch_username"
+}
+```
+
+**Good response**:
+
+```sh
+200 OK
+```
+
+**Bad response**:
+
+```sh
+403 Forbidden
+```
---
+++ server.js
@@ -0,0 +1,104 @@
+const fetch = require('node-fetch');
+const cryptoRandomString = require('crypto-random-string');
+var Datastore = require('nedb');
+var db = new Datastore();
+const html = require('node-html-parser');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3002;
+app.use(express.json());
+app.get('/', (req, res) => {
+  res.sendFile('landing.html', {
+    root: __dirname + '/public'
+  });
+});
+app.get('/style.css', (req, res) => {
+  res.sendFile('style.css', {
+    root: __dirname + '/public'
+  });
+});
+app.get('/api', (req, res) => {
+  res.sendStatus(200);
+});
+app.post('/api/init', (req, res) => {
+  if (req.body.user) {
+    db.find({
+      user: req.body.user
+    }, (err, docs) => {
+      if (docs[0]) {
+        db.remove({
+          user: req.body.user
+        }, {
+          multi: true
+        }, (err, numRemoved) => {
+          console.log(`Removed duplicate user ${req.body.user} from database`);
+        });
+      }
+    });
+    var code = cryptoRandomString({
+      length: 45,
+      type: 'distinguishable'
+    });
+    var doc = {
+      user: req.body.user,
+      code: code
+    }
+    db.insert(doc, (err, newDoc) => {
+      console.log(`Successfully created a verification code for user ${req.body.user}`);
+      res.send({
+        code: code
+      });
+    })
+  } else {
+    res.sendStatus(406);
+    console.log(req);
+  }
+});
+app.post('/api/verify', (req, res) => {
+  if (req.body.user) {
+    db.find({
+      user: req.body.user
+    }, (err, docs) => {
+      if (docs[0] !== undefined) {
+        console.log(`User ${req.body.user} exists in database`);
+        var code = docs[0].code;
+        fetch(`https://scratch.mit.edu/site-api/comments/user/${req.body.user}/?page=1`)
+          .then((res) => {
+            return res.text();
+          })
+          .then((data) => {
+            const root = html.parse(data);
+            var comment = {
+              plaintext: root.querySelector('.content').text.trim(),
+              username: root.querySelector('#comment-user').getAttribute('data-comment-user')
+            };
+            if (comment.plaintext == docs[0].code && comment.username == req.body.user) {
+              res.sendStatus(200);
+              db.find({
+                user: req.body.user
+              }, (err, docss) => {
+                if (docss[0]) {
+                  db.remove({
+                    user: req.body.user
+                  }, {
+                    multi: true
+                  }, (err, numRemoved) => {
+                    console.log(`Removed duplicate user ${req.body.user} from database`);
+                  });
+                }
+              });
+            } else {
+              res.sendStatus(403);
+            }
+          })
+      } else {
+        res.sendStatus(403);
+      }
+    })
+  } else {
+    res.sendStatus(406);
+  }
+});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
